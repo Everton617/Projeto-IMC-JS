376f2711701d919a1585ac14a9541088d33acfab
unknown <everton6943@gmail.com>
2021-08-09 21:10:32 -0300

    Projeto IMC JS

---
+++ JavaScript/aula12/ex012.js
@@ -0,0 +1,11 @@
+var hora = 12
+if ( hora < 12){
+    console.log('Bom dia')
+} else if ( hora <= 18) {
+    console.log(' Boa Tarde')
+} else {
+    console.log(' Boa noite ')
+} else if  ( hora <  12 && hora >= 1){
+    console.log('Boa madrugada')
+}
+
